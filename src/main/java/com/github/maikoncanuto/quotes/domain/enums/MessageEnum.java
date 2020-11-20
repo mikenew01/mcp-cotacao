77325404005d1f76c479c10084229684b55af1ad
Maikon Canuto <maikoncanuto@gmail.com>
2020-11-20 03:46:43 -0300

    removendo codigo desnecessario

--- src/main/java/com/github/maikoncanuto/quotes/domain/enums/MessageEnum.java
+++ src/main/java/com/github/maikoncanuto/quotes/domain/enums/MessageEnum.java
@@ -2,8 +2,6 @@ package com.github.maikoncanuto.quotes.domain.enums;
 
 public enum MessageEnum {
 
-    PROBLEMA_INTEGRACAO_MOEDAS_BACEN("Problema com a integração do serviço de moedas do Bacen."),
-    PROBLEMA_INTEGRACAO_COTACOES_BACEN("Problema com a integração do serviço de cotações do Bacen."),
     INTERCEPTOR_METODO_EXECUTADO("Método [%s] com parametros [%s] executado"),
     INTERCEPTOR_METODO_PROBLEMA("Problema na camada de serviço, método [%s] com parametros [%s] e erro: %s");
 
